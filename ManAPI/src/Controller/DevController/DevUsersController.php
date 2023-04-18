785cee647ea819624374a63ca8be89a8a4a11d2b
Yoan <yoan.sabatier@gmail.com>
2023-04-18 13:32:18 +0200

    Add: user edit password, delete one user

--- ManAPI/src/Controller/DevController/DevUsersController.php
+++ ManAPI/src/Controller/DevController/DevUsersController.php
@@ -4,9 +4,7 @@ namespace App\Controller\DevController;
 use Faker\Factory;
 use App\Entity\Users;
 use App\Repository\UsersRepository;
-use Doctrine\ORM\EntityManager;
 use Doctrine\ORM\EntityManagerInterface;
-use Doctrine\ORM\Mapping\Entity;
 use Symfony\Component\Serializer\Serializer;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
@@ -19,25 +17,25 @@ use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 
 class DevUsersController extends AbstractController
 {
-    private $usersPasswordHasher;
+    private UserPasswordHasherInterface $usersPasswordHasher;
+    private Serializer $serializer;
+    private array $encoders;
+    private array $normalizers;
 
     public function __construct(UserPasswordHasherInterface $usersPasswordHasher)
     {
         $this->usersPasswordHasher = $usersPasswordHasher;
+        $this->encoders[] = new JsonEncoder();
+        $this->normalizers[] = new ObjectNormalizer();
+        $this->serializer = new Serializer($this->normalizers,$this->encoders);
     }
 
     #[Route('/api/dev/users', name: 'dev_users', methods: ['GET'])]
     public function getUsers(UsersRepository $usersRepository): JsonResponse
     {
-        $encoders = [new JsonEncoder()];
-        $normalizers = [new ObjectNormalizer()];
-        
-        $serializer = new Serializer($normalizers,$encoders);
-        
         $users = $usersRepository->findAll();
         
-        
-        $JsonContent = $serializer->serialize($users, 'json');
+        $JsonContent = $this->serializer->serialize($users, 'json');
         return $this->json([
             Response::HTTP_OK, [], true,
             $JsonContent
@@ -48,17 +46,17 @@ class DevUsersController extends AbstractController
     public function createUsers(Request $request, int $number, EntityManagerInterface $manager): JsonResponse
     {
         $faker = Factory::create('fr_FR');
-
-        for ($i = 0; $i < $number; $i++) {
-
-            $users = new Users;
-            $users->setName($faker->userName());
-            $users->setEmail($faker->email());
-            $users->setPassword($this->usersPasswordHasher->hashPassword($users, "password"));
-
-            $manager->persist($users);
-        }
-
+        $hasher = $this->usersPasswordHasher;
+        for ($i = 0; $i < $number; $i++)
+        {   
+            $user = new Users;
+            $user->setName($faker->userName());
+            $user->setEmail($faker->email());
+            $user->setPassword($hasher->hashPassword($user, "password"));
+            
+            $manager->persist($user);
+        };
+        
         $manager->flush();
 
         return $this->json([
@@ -67,7 +65,7 @@ class DevUsersController extends AbstractController
         ]);
     }
 
-    #[Route('api/dev/users/user/{id<\d+>}/edit/{username<[a-zA-Z0-9]{8,32}>}', name:'dev_user_update-name', methods:['PUT'])]
+    #[Route('api/dev/users/user/{id<\d+>}/edit/username/{username<[a-zA-Z0-9]{8,32}>}', name:'dev_user_update-name', methods:['PUT'])]
     public function editUsername(Request $request, string $id , string $username, UsersRepository $usersRepository, EntityManagerInterface $manager): JsonResponse
     {
         $user = $usersRepository->find($id);
@@ -90,10 +88,10 @@ class DevUsersController extends AbstractController
             ]);
         }
     }
-    #[Route('api/dev/users/user/{id<\d+>}/edit/{usermail<[\w\-\.]+@[\w-]+\.+[\w-]{2,4}>}', name:'dev_user_update-mail', methods:['PUT'])]
+
+    #[Route('api/dev/users/user/{id<\d+>}/edit/mail/{usermail<[\w\-\.]+@[\w-]+\.+[\w-]{2,4}>}', name:'dev_user_update-mail', methods:['PUT'])]
     public function editUsermail(Request $request, string $id, string $usermail, UsersRepository $usersRepository, EntityManagerInterface $manager): JsonResponse
     {
-        // ! Faire REGEX pour email !
         $user = $usersRepository->find($id);
         if($user)
         {
@@ -114,6 +112,49 @@ class DevUsersController extends AbstractController
         ]);
     }
 
+    #[Route('api/dev/users/user/{id<\d+>}/edit/password/{password<[-a-zA-Z0-9]{16,}>}', name:'dev_user_update_password', methods:['PUT'])]
+    public function editUserPassword(Request $request, string $id, string $password, UsersRepository $usersRepository, EntityManagerInterface $manager): JsonResponse
+    {
+        $user = $usersRepository->find($id);
+
+        if($user)
+        {
+            $user->setPassword($this->usersPasswordHasher->hashPassword($user,$password));
+
+            $manager->persist($user);
+            $manager->flush();
+
+            return $this->json([
+                Response::HTTP_OK,
+                'content' => 'User password was changed.'
+            ]);
+        }
+        return $this->json([
+            Response::HTTP_NOT_FOUND,
+            'content' => "User not found."
+        ]);
+    }
+
+    #[Route('api/dev/users/user/{id<\d+>}/delete', name:'delete_one_user', methods:['DELETE'])]
+    public function deleteUser(Request $request, string $id, UsersRepository $usersRepository, EntityManagerInterface $entityManagerInterface): JsonResponse
+    {
+        $user = $usersRepository->find($id);
+
+        if ($user)
+        {
+            $usersRepository->remove($user);
+            $entityManagerInterface->flush();
+
+            return$this->json([
+                Response::HTTP_OK,
+                'content' => "User $id and all is data was deleted"
+            ]);
+        }
+        return $this->json([
+            Response::HTTP_NOT_FOUND,
+            'content' => 'User not found'
+        ]);
+    }
     #[Route('api/dev/users/delete', name:'delete_all_users', methods:['DELETE'])]
     public function deleteAllUsers(UsersRepository $usersRepository, EntityManagerInterface $entityManagerInterface): JsonResponse
     {
